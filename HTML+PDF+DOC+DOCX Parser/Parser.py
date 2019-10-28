344fec2a957d05cce2a64f8e26508192286a1d91
PanchenkoAnton <panchenkoantonvl@gmail.com>
2019-10-29 04:18:05 +1000

    Create Parser.py

---
+++ HTML+PDF+DOC+DOCX Parser/Parser.py
@@ -0,0 +1,9 @@
+class Parser:
+    """
+    Base class for all parsers.
+    """
+    def get_text(self, *args):
+        print("This method should be overridden in inherited classes!")
+
+    def get_links(self, *args):
+        print("This method should be overridden in inherited classes!")
