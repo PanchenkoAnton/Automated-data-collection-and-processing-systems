71b34e1d7a48dc61f19472fe40aa4136c846d759
PanchenkoAnton <iwannagithubsomething>
2019-10-29 14:57:49 +1000

    PDF links parsing has been added

--- Document_Parser/PDFParser.py
+++ Document_Parser/PDFParser.py
@@ -2,6 +2,7 @@ from Document_Parser import Parser
 from PyPDF2 import PdfFileReader
 
 import textract
+import re
 
 
 class PDFParser(Parser):
@@ -13,22 +14,29 @@ class PDFParser(Parser):
 
     def get_text(self):
         # return textract.process(self.filepath)
+        text = []
         with open(self.filepath, 'rb') as file:
             pdf = PdfFileReader(file)
             info = pdf.getDocumentInfo()
             pages_num = pdf.getNumPages()
-            print(info)
-            print(f'There are {pages_num} pages in that file')
+            # print(info)
+            # print(f'There are {pages_num} pages in that file')
             for i in range(pages_num):
                 page = pdf.getPage(i)
-                print(f'This is the text extracted from page #{i+1}:')
-                print(page.extractText())
+                # print(f'This is the text extracted from page #{i+1}:')
+                page_text = page.extractText()
+                # print(page_text)
+                text.append(page_text)
+        return text
 
     def get_links(self):
         links = []
+        link_re = re.compile(self.LINK_RE_PATTERN, (re.M | re.I))
         text = self.get_text()
+        for page in text:
+            for line in text:
+                result = link_re.search(line)
+                result_text = result.groups()
+                # print(result)
+                links.append(result_text)
         return links
-
-
-parser = PDFParser('test.pdf')
-print(parser.get_text())
