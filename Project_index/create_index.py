20c8a1e01605d4586220849495f45b3f5517a1a4
Alexey <alexey.ermolaev@smddev.com>
2019-12-09 13:01:29 +0300

    example tf

--- Project_index/create_index.py
+++ Project_index/create_index.py
@@ -1,6 +1,11 @@
 import asyncio
+import string
 
 from motor.motor_asyncio import AsyncIOMotorClient
+from nltk import FreqDist
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import RegexpTokenizer
 
 from Project_parser.parsers.HTMLParser import HTMLParser
 
@@ -9,13 +14,23 @@ async def do_find_one():
     db = AsyncIOMotorClient('localhost', 27000)['crawler']
     collection_msu = db.msu
     document = await collection_msu.find_one({"url": "https://www.msu.ru/"})
-    #print(document['data'])
     parser = HTMLParser(text=document['data'])
     text = parser.get_text()
-    # data = HTMLParser.get_text(text=document['data'])
-    # soup = BeautifulSoup(document['data'])
-    # data = soup.findAll(text=True)
-    print(text)
+    text = text.lower()
+
+    tokenizer = RegexpTokenizer(r'\w+')
+    tokens = tokenizer.tokenize(text)
+
+    tokens = [word for word in tokens if
+              word not in string.punctuation]
+    tokens = [word for word in tokens if word not in set(stopwords.words('russian'))]
+    tokens = [word for word in tokens if word not in set(stopwords.words('english'))]
+    wordnet_lemmatizer = WordNetLemmatizer()
+    print(tokens)
+    tokens = [wordnet_lemmatizer.lemmatize(word) for word in tokens]
+    print(tokens)
+    freq = FreqDist(tokens)
+    print(freq.most_common(5))
     return text
 
 
