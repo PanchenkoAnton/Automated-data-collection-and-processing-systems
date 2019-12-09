ac73f868ee3e13f77bc0a170ba39a4adf44fef18
PanchenkoAnton <iwannagithubsomething>
2019-12-09 22:17:47 +1000

    Deleted some unnecessary code

--- Project_index/create_index.py
+++ Project_index/create_index.py
@@ -21,14 +21,11 @@ async def do_find_one():
     document = await collection_msu.find_one({"url": "https://www.msu.ru/"})
     parser = HTMLParser(text=document['data'])
     text = parser.get_text()
-    # nlp = Russian()
 
     tmp_text = 'Здравствуйте, как у вас дела? Сегодня чудесная погодка у нас ' \
                'выдалась'
 
     nlp = spacy.load('ru2')
-    # nlp = spacy.load('ru2', disable=['tagger', 'parser', 'NER'])
-    # nlp.add_pipe(nlp.create_pipe('sentencizer'), first=True)
     tokenizer = RegexpTokenizer(r'\w+')
     tokens = tokenizer.tokenize(tmp_text)
     tmp_text = ' '.join(tokens)
@@ -39,23 +36,9 @@ async def do_find_one():
                 and s.lemma_ not in set(stopwords.words('english')):
             print(s.lemma_)
             lemmas.append(s.lemma_)
-        # print(list(['lemma "{}" from text "{}"'.format(t.lemma_, t.text)
-        #             for t in s]))
     freq = FreqDist(lemmas)
     print(freq.most_common(10))
 
-    # text = text.lower()
-    #
-    # tokens = [word for word in tokens if
-    #           word not in string.punctuation]
-    # tokens = [word for word in tokens if word not in set(stopwords.words('russian'))]
-    # tokens = [word for word in tokens if word not in set(stopwords.words('english'))]
-    # wordnet_lemmatizer = WordNetLemmatizer()
-    # print(tokens)
-    # tokens = [wordnet_lemmatizer.lemmatize(word) for word in tokens]
-    # print(tokens)
-    # freq = FreqDist(tokens)
-    # print(freq.most_common(5))
     return text
 
 
